c086a72e58a5f6dd9c004520b4759038186afe09
Gado.Remove <115067192+negative-jpeg@users.noreply.github.com>
2024-07-25 02:48:31 +0800

    pre-funtioning carousel

--- src/main.js
+++ src/main.js
@@ -1,2 +1,52 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
+
+function showSidemenu() {
+    const sidemenu = document.querySelector('.sidemenu');
+    const overlay = document.getElementById('overlay');
+    sidemenu.style.transform = 'translateX(0)';
+    overlay.style.display = 'block';
+}
+
+function closeSidemenu() {
+    const sidemenu = document.querySelector('.sidemenu');
+    const overlay = document.getElementById('overlay');
+    sidemenu.style.transform = 'translateX(-600px)';
+    overlay.style.display = 'none';
+}
+
+// Add event listeners to the open and close buttons
+document.getElementById('open').addEventListener('click', showSidemenu);
+document.getElementById('close').addEventListener('click', closeSidemenu);
+
+// Close the sidemenu when clicking outside of it
+document.getElementById('overlay').addEventListener('click', closeSidemenu);
+
+// Close the sidemenu when any item is clicked
+document.querySelectorAll('.sidemenu__items').forEach(item => {
+    item.addEventListener('click', closeSidemenu);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const categories = document.querySelector('.categories');
+    let isScrolling;
+
+    categories.addEventListener('scroll', function () {
+        clearTimeout(isScrolling);
+        categories.classList.remove('inactive-scrollbar');
+
+        isScrolling = setTimeout(function () {
+            categories.classList.add('inactive-scrollbar');
+        }, 500);
+    });
+});
+
+const carousel = document.querySelector(".carousel");
+
+carousel.addEventListener("wheel", function( e ) {
+    if ( e.wheelDelta > 0){
+        this.scrollLeft -= 50;
+    } else {
+        this.scrollLeft += 50;
+    }
+}, { passive: true});
